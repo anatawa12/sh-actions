41eac7bf10162621d96e770372aa56a01172eb76
anatawa12 <anatawa12@icloud.com>
2022-12-22 20:12:03 +0900

    feat: add create-unitypackage

---
+++ create-unitypackage/main.bash
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -eu
+
+main() {
+    # parameter check
+    test -z "${OUTPUT:-}" && die "no OUTPUT"
+    test -z "${ROOT:-}" && die "no ROOT"
+    : "${PREFIX:="$(relativize_path "$ROOT")"}"
+
+    # prepare other variables
+    create_temp_dir create-unitypackage
+
+    local ROOT_META meta_path TAR_ROOT file_path GUID
+
+    TAR_ROOT="$TEMPDIR/tar_root"
+    mkdir "$TAR_ROOT"
+
+  pushd "$ROOT" > /dev/null
+    # find root meta file. maybe used later. ROOT_META is empty if not found.
+    ROOT_META="../$(basename "$ROOT").meta"
+    test ! -f "$ROOT_META" && ROOT_META=""
+
+    # find all files in the directory
+    find "." -name "*.meta" | sed 's#^\./##' | while IFS="" read -r meta_path; do
+        file_path="${meta_path%%.meta}"
+        GUID="$(read_guid "$meta_path")"
+
+        debugf '%s for %s (guid: %s)' "$meta_path" "$file_path" "$GUID"
+
+        if [ -z "$GUID" ]; then
+            warnf "GUID for %s not found!" "$meta_path"
+            continue
+        fi
+        if [ ! -e "$file_path" ]; then
+            warnf "Asset file/directory for %s not found!" $meta_path
+            continue
+        fi
+
+        mkdir "$TAR_ROOT/$GUID"
+        cp "$meta_path" "$TAR_ROOT/$GUID/asset.meta"
+        # copy asset only if the asset is file
+        test -f "$file_path" && cp "$file_path" "$TAR_ROOT/$GUID/asset"
+        printf "%s/%s" "$PREFIX" "$file_path" > "$TAR_ROOT/$GUID/pathname"
+    done
+  popd > /dev/null
+
+    # if exists, add meta file for package root
+    if [ -n "$ROOT_META" ]; then
+        debugf "%s for root folder found\n" "$ROOT_META"
+        GUID="$(read_guid "$meta_path")"
+
+        if [ -z "$GUID" ]; then
+            warnf "GUID for %s not found!" "$meta_path"
+        else
+            mkdir "$TAR_ROOT/$GUID"
+            cp "$ROOT_META" "$TAR_ROOT/$GUID/asset.meta"
+            printf "%s" "$PREFIX" > "$TAR_ROOT/$GUID/pathname"
+        fi
+    fi
+
+    # create unitypackage file with tar
+    tar -czf "$OUTPUT" -C "$TAR_ROOT" '.'
+}
+
+read_guid() {
+    grep '^guid: ' "$1" | head -1 | sed 's#^guid: *##'
+}
+
+. "$(dirname $0)/../utils.bash"
+
+# execute
+main
---
+++ utils.bash
@@ -0,0 +1,62 @@
+# sets TEMPDIR global variable
+create_temp_dir() {
+    TEMPDIR="$(mktemp -d "${TMPDIR:-/tmp}/${1:-temp}.XXXXXX")"
+    debugf "temp: $TEMPDIR"
+    #trap 'rm -rf $TEMPDIR' EXIT
+}
+
+#region logging
+# echo to stderr and exit
+die () {
+    echo "$@" >&2
+    exit 1
+}
+
+printf_prefix() {
+    local PREFIX="$1"
+    local FMT="$2"
+    shift
+    shift
+    printf "$PREFIX$FMT\n" "$@"
+}
+
+warnf () {
+    printf_prefix "::warning::" "$@" >&2
+}
+
+debugf () {
+    printf_prefix "::debug::" "$@"
+}
+
+#endregion
+
+#region path utils
+
+relativize_path() {
+    local COMPONENTS JOINED
+    # first, remove `[^/]+/../`
+    IFS="/" read -r -a COMPONENTS <<< "$1"
+
+
+    for index in "${!COMPONENTS[@]}" ; do
+        if [ "${COMPONENTS[index]}" = '.' ]; then
+            COMPONENTS[index]=''
+        elif [ "${COMPONENTS[index]}" = '..' -a "$index" -ne 0 ]; then
+            # path/something////../path
+            # -> path/////../path
+            for prev in `seq $(($index - 1)) 0`; do
+                if [ -n "${COMPONENTS[prev]}" ]; then
+                    COMPONENTS[prev]=''
+                    break
+                fi
+            done
+            COMPONENTS[index]=''
+        fi
+    done
+    JOINED="$(IFS="/"; echo "${COMPONENTS[*]}")"
+
+    # remove multiple / and leading/heading /
+    echo "$JOINED" | sed -e 's#//*#/#g' -e 's#^/##' -e 's#/$##'
+}
+
+#endregion
