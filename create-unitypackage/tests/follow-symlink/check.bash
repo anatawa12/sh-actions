66e087c4d9d60e42f0578960cdeeeb7df917b14f
anatawa12 <anatawa12@icloud.com>
2023-02-21 22:24:33 +0900

    fix(test/create-unitypackage/follow-symlink): fix test again

--- create-unitypackage/tests/follow-symlink/check.bash
+++ create-unitypackage/tests/follow-symlink/check.bash
@@ -8,5 +8,5 @@ source ./create-unitypackage/tests/tests.bash
 CONTENTS="create-unitypackage/tests/follow-symlink/contents"
 
 check_file_asset   "2323c8d8e5fb402b9fdbc30bb0235b4c" "test.cs"
-check_file_asset   "395c349e3ab442ffb639bbe5be174bb4" "folder"
-check_folder_asset "42962cc3730a4cd286f19db25b321d98" "folder/content.txt"
+check_folder_asset "395c349e3ab442ffb639bbe5be174bb4" "folder"
+check_file_asset   "42962cc3730a4cd286f19db25b321d98" "folder/content.txt"
